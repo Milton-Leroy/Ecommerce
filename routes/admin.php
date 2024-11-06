fb628b1e72be37530688a9240aca8a569edfbed5
Miltech <yengongmilton58@gmail.com>
2024-11-06 01:03:16 +0100

    Started working on category feature

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,6 @@ Route::post('profile/update/password', [ProfileController::class, 'updatePasswor
 
 /* slider Routes */
 Route::resource('slider', SliderController::class);
+
+/* Category Routes */
+Route::resource('category', CategoryController::class);
